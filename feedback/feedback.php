9812116645cc55d51a0b1291028d9a82a267716f
unknown <up649230@myport.ac.uk>
2017-01-16 15:22:22 +0000

    Feedback back-end inception

--- feedback/feedback.php
+++ feedback/feedback.php
@@ -1,10 +1,37 @@
 <?php
-header("Access-Control-Allow-Origin: *");
 
-$conn = new mysqli("localhost", "root", "", "visitorapp");
+$postData = file_get_contents("php://input");
+$request = json_decode($postData);
+$question1 = $request->question1;
+$question2 = $request->question2;
+$question2comment = $request->question2comment;
+$question3 = $request->question3;
+$question3comment = $request->question3comment;
+$question4 = $request->question4;
+$question5 = $request->question5;
+$question5comment = $request->question5comment;
+$question6 = $request->question6;
+$question7 = $request->question7;
 
-$sql = "INSERT INTO feedback (question1, question2, question2comment, question3, question3comment, question4, question5, question5comment, question6, question7)
-     VALUES ()";
+$server = "localhost";
+$user = "root";
+$pass = "";
+$db = "visitorapp";
+
+$conn = new mysqli($server, $user, $pass, $db);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connection_error);
+}
+
+$sql = "INSERT INTO feedback ('question1', 'question2', 'question2comment', 'question3', 'question3comment', 'question4', 'question5', 'question5comment', 'question6', 'question7')
+     VALUES ($question1, $question2, $question2comment, $question3, $question3comment, $question4, $question5, $question5comment, $question6, $question7)";
+
+if ($conn->query($sql$ === TRUE) {
+    echo "Feedback submitted";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
 
 $conn->close();
 
--- js/feedback.js
+++ js/feedback.js
@@ -1,4 +1,4 @@
-app.controller('feedbackController', ['$scope', function($scope) {
+app.controller('feedbackController', ['$scope', '$http', function($scope, $http) {
 
     
     $scope.feedbackq1 = ["Wednesday 24th February 2017", "Saturday 27th February 2017", "Wednesday 2nd March 2017", "Wednesday 9th March 2017", "Saturday 12th March 2017", "Wednesday 23rd March 2017", "Saturday 23rd April 2017"];
@@ -10,7 +10,7 @@ app.controller('feedbackController', ['$scope', function($scope) {
         { label: 'Not useful', value: 'Not useful' },
         ];
     $scope.q2 = '';
-    $scope.q2q = '';
+    $scope.q2c = '';
     
     $scope.feedbackq3 = [
         { label: 'Very useful', value: 'Very useful' },
@@ -18,7 +18,7 @@ app.controller('feedbackController', ['$scope', function($scope) {
         { label: 'Not useful', value: 'Not useful' },
         ];
     $scope.q3 = '';
-    $scope.q3q = '';
+    $scope.q3c = '';
    
     $scope.feedbackq4 = [
         { label: 'First', value: 'First' },
@@ -33,9 +33,41 @@ app.controller('feedbackController', ['$scope', function($scope) {
         { label: "I'm less likely to enrol", value: "I'm less likely to enrol" },
         ];
     $scope.q5 = '';
-    $scope.q5q = '';
+    $scope.q5c = '';
 
     $scope.q6 = '';
     $scope.q7 = '';
+
+    $scope.submitFeedback = function() {
+    
+        var request = $http({
+            method: "post",
+            url: "feedback/feedback.php",
+            data: {
+                question1: $scope.q1,
+                question2: $scope.q2,
+                question2comment: $scope.q2c,
+                question3: $scope.q3,
+                question3comment: $scope.q3c,
+                question4: $scope.q4,
+                question5: $scope.q5,
+                question5comment: $scope.q5c,
+                question6: $scope.q6,
+                question7: $scope.q7,
+            },
+            headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
+        });
+
+        console.log($scope.q1); 
+        console.log($scope.q2); 
+        console.log($scope.q2c); 
+        console.log($scope.q3); 
+        console.log($scope.q3c); 
+        console.log($scope.q4); 
+        console.log($scope.q5); 
+        console.log($scope.q5c); 
+        console.log($scope.q6); 
+        console.log($scope.q7); 
+    };
 
 }]);
